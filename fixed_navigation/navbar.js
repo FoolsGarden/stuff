da7bfc0ccde6584386c016e7b7ff36baeba7fc32
codeamac06 <codeamac06@gmail.com>
2016-10-12 10:19:55 -0500

    nav bar fixed

---
+++ fixed_navigation/navbar.js
@@ -0,0 +1,14 @@
+var divs = $('.top-nav'),
+    limit = 35;  /* scrolltop value when opacity should be 0 */
+
+$(window).on('scroll', function() {
+   var st = $(this).scrollTop();
+
+   /* avoid unnecessary call to jQuery function */
+   if (st <= limit) {
+      divs.css({ 'opacity' : (1 - st/limit) });
+   }
+});
+
+
+
