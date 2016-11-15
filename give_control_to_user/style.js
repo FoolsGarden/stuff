1a916eeee7b0f6062e3dc4840b399ef153d5e973
codeamac07 <codeamac07@gmail.com>
2016-11-15 12:07:26 -0600

    terminando give control

--- give_control_to_user/style.js
+++ give_control_to_user/style.js
@@ -1,3 +1,15 @@
-$(function(){
-
+$( document ).ready(function() {
+    $(this).find('input[name="selector"]').focus();
+    $('#style_editor').submit(function(event){
+      event.preventDefault(); 
+      var selector = $(this).find('input[name="selector"]').val();
+      var property = $(this).find('input[name="property"]').val();
+      var value = $(this).find('input[name="value"]').val();
+      console.log("Selector: "+selector+" Property: "+property+" Value: "+value);
+      $(selector).css(property,value);
+      $(this).find('input[name="selector"]').val("");
+      $(this).find('input[name="property"]').val("");
+      $(this).find('input[name="value"]').val("");
+      $(this).find('input[name="selector"]').focus();
+    });
 });
