0ad75f0aa56fa45112fe4cfe1256befad89293db
codeamac07 <codeamac07@gmail.com>
2016-11-18 12:46:48 -0600

    tweet ahora

--- API_T/app/controllers/index.rb
+++ API_T/app/controllers/index.rb
@@ -1,16 +1,17 @@
+#=======================================================================
 get '/' do
-  # La siguiente linea hace render de la vista 
-  # que esta en app/views/index.erb
   erb :index
 end
-
+#=======================================================================
 post '/fetch' do 
   if CLIENT.user_exists?(params[:user])
-    redirect "/#{params[:user]}"
+     redirect "/#{params[:user]}"
+     #params[:user]
   else
-    "Error"  
+     nil 
   end
 end
+#=======================================================================
 get '/:handle' do
   @user = TwitterUser.find_or_create_by(name:params[:handle])
   if @user
@@ -27,7 +28,26 @@ get '/:handle' do
   end
   erb :tweets , layout: false
 end
+#=======================================================================
+post '/tweet' do
+  tweet = params[:tweet]
+  if tweet 
+    CLIENT.update("#{tweet}")
+    tweet_web = CLIENT.get_all_tweets("molacfar").first.text
+    if tweet == tweet_web
+      return "<p style='color:green'>#{params[:tweet]}</p>"
+    else
+      return nil  
+    end
+  else
+    return nil
+  end
+    # Recibe el input del usuario
 
+    # Crea el tweet utilizando la API de Twitter
 
+    # Regresa al usuario el tweet o un mensaje de EXITO o ERROR
+end
+#=======================================================================
 
 
--- API_T/public/js/application.js
+++ API_T/public/js/application.js
@@ -1,7 +1,64 @@
 $(document).ready(function() {
-	// Este código corre después de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
+  console.log("ready!");
+  $('#frm_tweets').hide();
+  $('#frm_make_tw').hide();
+//==============================================================  
+  $('body').on('submit','#frm_tweets',function(event){
+    event.preventDefault();
+    var $form = $(this)
+    var cadena = $(this).serialize();
+    $.post('/fetch',cadena,function(data){
+      if (data) {
+        $form.find('input[name="user"]').val("");
+        $('#wait').html(data);
+        $form.find('input[name="user"]').focus();
+        //$(location).attr('href','/'+data);
+      }else{
+        alert("Error");
+      }
+    });
+    $('#wait').html('<h2>Please wait...</h2>');
+    $('#wait').append('<img src="/spinner.gif" height="200" width="250">');
 
+  });
+//==============================================================
+$('body').on('submit','#frm_make_tw',function(event){
+    event.preventDefault();
+    var $form = $(this)
+    var esto = $(this).find('textarea[name="tweet"]').val();
+    var cadena = $(this).serialize();
+    if (esto.length < 120) {
+      $.post('/tweet',cadena,function(data){
+        if (data) {
+          $form.find('textarea[name="tweet"]').val("");
+          $('#wait').html(data);
+          $form.find('textarea[name="tweet"]').focus();    
+        }else{
+          alert("Error making tweet");
+        }
+      });
+      $('#wait').html('<h2>Please wait...</h2>');
+      $('#wait').append('<img src="/spinner.gif" height="200" width="250">');
+    }else{
+      alert("No more than 120 characters")
+    };
+
+  }); 
+//==============================================================  
+  $(document).on("pageload",function(){
+    alert("pageload event fired - the external page has been successfully loaded and inserted into the DOM!");
+  });;
+//==============================================================  
+  $('body').on('click','#btn_tweets',function(event){
+    $('#wait').html("");
+    $('#frm_make_tw').hide();
+    $('#frm_tweets').slideToggle();
+  });
+//============================================================== 
+  $('body').on('click','#btn_make_tw',function(event){
+    $('#wait').html("");
+    $('#frm_tweets').hide();
+    $('#frm_make_tw').slideToggle();
+  });
+//==============================================================  
 });
